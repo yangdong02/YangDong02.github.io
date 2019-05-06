2513b7b796a5fd2e3d42bf793c0f62e8efdf571f
yang2002 <1428873902@qq.com>
2019-05-06 17:15:34 +0800

    Site updated: 2019-05-06 17:15:14

--- js/src/post-details.js
+++ js/src/post-details.js
@@ -103,11 +103,12 @@ $(document).ready(function() {
   }
 });
 
-// $(document).ready(function(){/* 
-    // $(document).on('click', '.fold_hider', function(){
-        // $('>.fold', this.parentNode).slideToggle();
-        // $('>:first', this).toggleClass('open');
-    // });
-    默认情况下折叠
-    // $("div.fold").css("display","none");
-// }); */
+
+$(document).ready(function(){
+    $(document).on('click', '.fold_hider', function(){
+        $('>.fold', this.parentNode).slideToggle();
+        $('>:first', this).toggleClass('open');
+    });
+    //默认情况下折叠
+    $("div.fold").css("display","none");
+});
